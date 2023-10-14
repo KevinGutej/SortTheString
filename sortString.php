b310fb4850f31ecb98c2230337e980ca343377e8
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-10-14 23:26:38 +0200

    Creating a function that will sort the string
    e.g "eA2a1E ➞ "aAeE12"

---
+++ sortString.php
@@ -0,0 +1,26 @@
+<?php
+
+
+function customSort($string) {
+    $lowercaseChars = $uppercaseChars = $numbers = '';
+
+    for ($i = 0; $i < strlen($string); $i++) {
+        $char = $string[$i];
+        if (ctype_lower($char)) {
+            $lowercaseChars .= $char;
+        } elseif (ctype_upper($char)) {
+            $uppercaseChars .= $char;
+        } elseif (is_numeric($char)) {
+            $numbers .= $char;
+        }
+    }
+
+    $lowercaseChars = str_split($lowercaseChars);
+    $uppercaseChars = str_split($uppercaseChars);
+    $numbers = str_split($numbers);
+
+    sort($lowercaseChars);
+    sort($uppercaseChars);
+    sort($numbers);
+
+}
